6a8f3ec95fae31b3356f3a68912841172bfe8fba
Karolis <55600701+KarolisPa@users.noreply.github.com>
2021-12-13 08:59:17 +0200

    paskutinis ikelimas

--- resources/views/livewire/forma.blade.php
+++ resources/views/livewire/forma.blade.php
@@ -77,9 +77,4 @@
         </form>
             <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" type="button" wire:click="edit()">Redaguoti</button>
         @endif
-    @if (session()->has('message'))
-        <div class="alert alert-success">
-            {{ session('message') }}
-        </div>
-    @endif
 </div>
